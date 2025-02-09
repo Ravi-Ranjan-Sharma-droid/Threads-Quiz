a8313fe352bdc6cb18ed697e00d692bd68188113
Ravi Ranjan Sharma <rr2436310@gmail.com>
2025-02-09 12:57:39 +0000

    quiz solved 1

---
+++ 01_javaScript-quiz/01_quiz.js
@@ -0,0 +1,9 @@
+//Will the following JavaScript code print numbers from 1 to 100?
+
+
+let a = 1;
+for (let i = 0; i < 100; i++) {
+    console.log(a + i);
+}
+
+/*The code starts by setting a number to one. Then, it repeats a process one hundred times. In each repetition, it adds the current count to the starting number and shows the result. The first result is one, and each following result increases by one until it reaches one hundred.*/
