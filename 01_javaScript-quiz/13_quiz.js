a105cfcf2ee965da51dc95c76879c302a9ce2019
Ravi Ranjan Sharma <rr2436310@gmail.com>
2025-03-02 10:59:16 +0000

    13_quiz hint

---
+++ 01_javaScript-quiz/13_quiz.js
@@ -0,0 +1,27 @@
+let count = 0;
+const add = () => {
+count++;
+return function () {
+return count * 2;
+};
+};
+const result = add();
+console.log(result());
+console.log(result());
+
+/*
+
+  let count = 0;
+
+const add = () => {
+    count++; // Increments the global `count` variable
+    return function () {
+        return count * 2;
+    };
+};
+
+const result = add(); // Calls `add()` and assigns the returned function to `result`
+console.log(result()); // Calls the function returned by `add()`
+console.log(result()); // Calls the function again
+  
+
